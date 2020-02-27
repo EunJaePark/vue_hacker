da476ef9925929b729a5409dd6056e5da743b890
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-02-27 22:51:19 +0900

    hacker
    - promise방식에 대한 설명 추가.

--- hacker/src/store/index.js
+++ hacker/src/store/index.js
@@ -14,7 +14,9 @@ export const store = new Vuex.Store({  //actions이 mutations을 실행해주고
     }
   },
   actions: {
-    // api/index.js에 데이터 가져오는 행동대장들을 호출해서 받고, 뮤테이션에 넘김   // promise 방식
+    // api/index.js에 데이터 가져오는 행동대장들을 호출해서 받고, 뮤테이션에 넘김   
+	// axios는 "promise 방식"을 기반으로 api를 제공한다.
+	// : 어떤 작업이 성공했을 때(resolve), promise 객체의 then() 함수에 넘겨진 파라미터(함수)를 단 한번만 호출하겠다는 약속입니다. 또한 실패했을 경우(reject)에도 catch()함수를 통해서 실패 이후의 작업을 처리할 수 있습니다. 
     FETCH_LIST(context, pageName) {  //두개의 인자를 받아야함. context는 정해져 있는 것(자동으로 생겨서 받아오게 되는 것). 
       return fetchListItem(pageName)
       .then(res => {
